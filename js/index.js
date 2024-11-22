c4f77ba4ead040bda5070d3928e3317ca0c4f771
Arjun Dasgupta <dasgupta1arjun@gmail.com>
2024-11-22 13:55:48 -0500

    semi completed input functionality

--- js/index.js
+++ js/index.js
@@ -1,10 +1,38 @@
-const letter = document.querySelector('letter');
-console.log(letter)
+const letter = document.querySelectorAll('.letter');
+const answerArr = [];
 
 function isLetter(letter) {
     return /^[a-zA-Z]$/.test(letter);
 }
 
-//function init() {
-    //document.querySelector('')
-//}
+async function verify() {
+    let answerStr = ''
+    for (let i = 0; i < answerArr.length; i++) {
+        answerStr += answerArr[i];
+    }
+}
+
+async function init() {
+    document.addEventListener('keyup', function letterVerification (event) {
+        const keyStroke = event.key;
+        console.log(keyStroke)
+
+        if (keyStroke === 'Enter') {
+            if (answerArr.length === 5) {
+                // letters[index].innerText = keyStroke;
+                verify();
+            }
+        } else if (keyStroke === 'Backspace') {
+            if (answerArr.length > 0) {
+                answerArr.pop()
+            }
+        } else if (isLetter(keyStroke)) {
+            if (answerArr.length < 5) {
+                answerArr.push(keyStroke.toUpperCase());
+            } 
+        } 
+        console.log(answerArr)
+    })
+}
+
+init();
